befb0ee2d01f0026d28fa14ae84e68d0f6dd61da
Nikita Razinkov <m31k0l2@gmail.com>
2019-06-16 17:21:47 +0300

    fix menu table

--- src/demo.js
+++ src/demo.js
@@ -1,47 +1,65 @@
-window.Quill = require('quill');
-const quillTable = require('./index.js');
+window.Quill = require("quill");
+const quillTable = require("./index.js");
 
 Quill.register(quillTable.TableCell);
 Quill.register(quillTable.TableRow);
 Quill.register(quillTable.Table);
 Quill.register(quillTable.Contain);
-Quill.register('modules/table', quillTable.TableModule);
+Quill.register("modules/table", quillTable.TableModule);
 
-import 'quill/dist/quill.core.css';
-import 'quill/dist/quill.snow.css';
-import './css/quill.table.css';
+import "quill/dist/quill.core.css";
+import "quill/dist/quill.snow.css";
+import "./css/quill.table.css";
 
 const maxRows = 10;
 const maxCols = 5;
 const tableOptions = [];
 for (let r = 1; r <= maxRows; r++) {
     for (let c = 1; c <= maxCols; c++) {
-        tableOptions.push('newtable_' + r + '_' + c);
+        tableOptions.push("newtable_" + r + "_" + c);
     }
 }
 
 const defaultToolbar = [
-    [{ table: ["newtable_1_2", "newtable_1_3", "newtable_1_4", "newtable_2_2", "newtable_2_3", "newtable_3_3"] }, { table: 'append-row' }, { table: 'append-col' }, { table: 'insert' }, { table: 'remove-col' }, { table: 'remove-row' }, { table: 'remove-table' }],    ['bold', 'italic', 'underline', 'strike'],
-    ['blockquote', 'code-block', 'image'],
-
-    [{'list': 'ordered'}, {'list': 'bullet'}],
-
-    [{'indent': '-1'}, {'indent': '+1'}],
-    [{'header': [1, 2, 3, 4, 5, 6, false]}],
-
-    [{'color': []}, {'background': []}],
-    [{'font': []}],
-    [{'align': []}],
-
-    ['clean']
-];
-const Editor = new Quill(document.getElementById('quillContainer'), {
-        modules: {
-            toolbar: defaultToolbar,
-            table: true
+    [
+        {
+            table: [
+                "newtable_1_2",
+                "newtable_1_3",
+                "newtable_1_4",
+                "newtable_2_2",
+                "newtable_2_3",
+                "newtable_3_3",
+                "remove-table"
+            ]
         },
-        placeholder: '',
-        theme: 'snow',
-        readOnly: false,
-    }
-);
+        { table: "append-row" },
+        { table: "append-col" },
+        { table: "insert" },
+        { table: "remove-col" },
+        { table: "remove-row" },
+        { table: "remove-table" }
+    ],
+    ["bold", "italic", "underline", "strike"],
+    ["blockquote", "code-block", "image"],
+
+    [{ list: "ordered" }, { list: "bullet" }],
+
+    [{ indent: "-1" }, { indent: "+1" }],
+    [{ header: [1, 2, 3, 4, 5, 6, false] }],
+
+    [{ color: [] }, { background: [] }],
+    [{ font: [] }],
+    [{ align: [] }],
+
+    ["clean"]
+];
+const Editor = new Quill(document.getElementById("quillContainer"), {
+    modules: {
+        toolbar: defaultToolbar,
+        table: true
+    },
+    placeholder: "",
+    theme: "snow",
+    readOnly: false
+});
